bf8d24fd9ff912ce08ada721e46f5c65059170f0
Duleetha Madubhashana <148762717+Duleetha2@users.noreply.github.com>
2025-02-12 09:11:54 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Project02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+            int result;
+
+            //Inputs
+            Console.Write("Enter First Number :");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Second Number :");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            //Addition
+            result = num1 + num2;
+            Console.WriteLine("Addition :" + result);
+
+            //Substraction
+            result = num1 - num2;
+            Console.WriteLine("Substraction :" + result);
+
+            Console.ReadLine();
+        }
+    }
+}
